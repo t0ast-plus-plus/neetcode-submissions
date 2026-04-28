7e032a9b4c2df7968c309fd75c5a487e861c77b4
t0ast <44268536+t0ast-plus-plus@users.noreply.github.com>
2026-04-28 11:48:17 -0700

    Add: graph - submission-4

---
+++ Data Structures & Algorithms/graph/submission-4.java
@@ -0,0 +1,59 @@
+class Graph {
+    // key = vertex ID, value = edge destinations
+    private final HashMap<Integer, HashSet<Integer>> graph;
+
+    public Graph() {
+        graph = new HashMap<>();
+    }
+
+    public void addEdge(final int src, final int dst) {
+        // add src vertex to graph
+        graph.putIfAbsent(src, new HashSet<>());
+        // add dst vertex to graph
+        // note: technically optional if adequate null checks are in place
+        // graph.putIfAbsent(dst, new HashSet<>());
+        // add edge from src to dst (won't duplicate because Set)
+        graph.get(src).add(dst);
+    }
+
+    public boolean removeEdge(final int src, final int dst) {
+        final HashSet<Integer> vertex = graph.get(src);
+        if (vertex == null) {
+            // vertex does not exist
+            return false;
+        }
+
+        // remove returns true if actual remove occurred, otherwise false
+        return vertex.remove(dst);
+    }
+
+    public boolean hasPath(final int src, final int dst) {
+        // initiate DFS from src, seeking dst, starting with no nodes visited
+        return seek(src, dst, new HashSet<>());
+    }
+
+    private boolean seek(final int src, final int dst, final HashSet<Integer> visited) {
+        if (src == dst) {
+            // path found
+            return true;
+        }
+
+        final HashSet<Integer> vertex = graph.get(src);
+        if (vertex == null || visited.contains(src)) {
+            // dead end or loop
+            return false;
+        }
+
+        // mark src as visited then keep looking for dst
+        visited.add(src);
+        for (final int toVisit : vertex) {
+            if (seek(toVisit, dst, visited)) {
+                // found somewhere on this path
+                return true;
+            }
+        }
+
+        // not found
+        return false;
+    }
+}
