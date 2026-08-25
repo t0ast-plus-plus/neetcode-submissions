417b2c7ad6698629e70af572022c049322a886b8
t0ast <44268536+t0ast-plus-plus@users.noreply.github.com>
2026-08-25 13:52:02 -0700

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,59 @@
+class MedianFinder {
+    final PriorityQueue<Integer> high;
+    final PriorityQueue<Integer> low;
+
+    public MedianFinder() {
+        // top half of values sorted in default ascending order
+        high = new PriorityQueue<>();
+        // bottom half of values sorted in descending (reverse default) order
+        low = new PriorityQueue<>(Collections.reverseOrder()); // descending order (reversed default)
+    }
+    
+    public void addNum(final int num) {
+        // add to high half if greater than its lowest value (head), otherwise add to the low half
+        if(!high.isEmpty() && high.peek() < num) {
+            high.add(num);
+        } else {
+            low.add(num);
+        }
+
+        // System.out.println("\nADDED: " + num);
+        // System.out.println("LOW: " + low);
+        // System.out.println("HIGH: " + high);
+        
+        // rebalance the size of the two halves as needed
+        rebalance();
+    }
+    
+    public double findMedian() {
+        // equal size = even quantity of elements, so median = average of middle two
+        if(high.size() == low.size()) {
+            return ((double) high.peek() + (double) low.peek()) / 2.0;
+        } 
+        
+        // otherwise, the half with the most elements will hold the median at its head
+        if (high.size() > low.size()) {
+            return (double) high.peek();
+        } else {
+            return (double) low.peek();
+        }
+    }
+
+    private void rebalance() {
+        // no rebalancing needed if sizes are within 1 of each other
+        if(Math.abs(high.size() - low.size()) <= 1) {
+            return;
+        }
+
+        // move one element from the larger (by quantity) half into the other half to equalize sizes
+        if(high.size() > low.size()) {
+            low.add(high.poll());
+        } else {
+            high.add(low.poll());
+        }
+
+        // System.out.println("\nREBALANCED: ");
+        // System.out.println("LOW: " + low);
+        // System.out.println("HIGH: " + high);
+    }
+}
